107933c63cdd7e880f6a7b89efa04a2ad5045fc7
Gheorghi Isac <GheorghiI@neogames.com>
2024-08-19 16:27:41 +0300

    Added godoc

--- ch10.go
+++ ch10.go
@@ -1,5 +1,9 @@
+// Package ch10 contains a function Add to add two integers
+
 package ch10
 
+// Add takes two integers and returns its sum.
+// According to rules described in https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
